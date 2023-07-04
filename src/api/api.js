b446409df518bc0650e55eff01c814c9ae5cada8
Marcus-devfs <marcusvini6277@gmail.com>
2023-07-04 16:07:24 -0300

    retirada da url da api

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,7 @@
 import axios from 'axios'
 
-//LOCAL
 export const api = axios.create({
-   // baseURL: 'http://localhost:3000',
    baseURL: process.env.NEXT_PUBLIC_API_URL
 })
 
-//QUENTE
-
-//  export const api = axios.create({
-//      baseURL: 'https://melies-api.onrender.com',
-//  })
 
