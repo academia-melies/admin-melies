5f0c3d85a040210e0cbcf4afc2ea74b3ac37fada
Marcus-devfs <marcusvini6277@gmail.com>
2023-08-15 19:05:46 -0300

    select all permissions

--- src/pages/administrative/users/[id].js
+++ src/pages/administrative/users/[id].js
@@ -1357,6 +1357,7 @@ export default function EditUser() {
                                 onSelect={(value) => {
                                     setForeigner(value)
                                     setUserData({ ...userData, nacionalidade: value === true ? 'Estrangeira' : 'Brasileira Nata' })
+                                    console.log(value)
                                 }}
                                 sx={{ width: 1 }} />
                         </Box>
