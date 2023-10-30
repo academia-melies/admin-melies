e185de03a833767273aa7a0ec09dff73de3c0269
Marcus-devfs <marcusvini6277@gmail.com>
2023-10-30 16:27:54 -0300

    add excluse script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
    return (
       <Html lang="en">
-         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
          <Head />
          <body>
             <Main />
