fb647ef9168e1ec732ca0754032e7d9fb126054f
Marcus-devfs <marcusvini6277@gmail.com>
2024-08-22 09:29:18 -0300

    retirado funcao do list user

--- src/pages/administrative/users/list.js
+++ src/pages/administrative/users/list.js
@@ -408,15 +408,6 @@ export default function ListUsers(props) {
     )
 }
 
-const fetchPermissions = async () => {
-    try {
-        const actions = await checkUserPermissions(router, userPermissions, menuItemsList)
-        setIsPermissionEdit(actions)
-    } catch (error) {
-        console.log(error)
-        return error
-    }
-}
 const styles = {
     containerRegister: {
         display: 'flex',
